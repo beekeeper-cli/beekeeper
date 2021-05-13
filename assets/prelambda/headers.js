d3ec1471d93dbb3347fbcea76627179668017c9f
Ian Eustis <eustisic@gmail.com>
2021-05-13 12:39:21 -0700

    updated package version

---
+++ assets/prelambda/headers.js
@@ -0,0 +1,15 @@
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Origin': S3_URL,
+  'Access-Control-Allow-Headers': '*',
+  'Access-Control-Allow-Methods': '*'
+};
+
+
+fetch(API_URL, {'credentials': "include"});
+
+
+
+
+
